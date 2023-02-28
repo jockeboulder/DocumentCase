f9cac7e3a652f1b33eb1c0b7219acf4a56c5bc12
jockeboulder <joakim.anneback@boulder.se>
2023-02-28 18:15:28 +0100

    fix missing using

--- DocumentService/App/Clients/PdfClient.cs
+++ DocumentService/App/Clients/PdfClient.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Text;
 
+namespace Clients;
 public class PdfClient
 {
     public static async Task<HttpResponseMessage> CreateAsync(Guid documentNumber, string customerNumber, string documentText)
--- DocumentService/App/Controllers/DocumentController.cs
+++ DocumentService/App/Controllers/DocumentController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Clients;
 
 namespace DotNet.Docker.Controllers;
 
