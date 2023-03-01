db276f4df682fb7dcdd54ea61c88818130854f15
jockeboulder <joakim.anneback@boulder.se>
2023-03-01 08:03:19 +0100

    Add extra documentation

--- DocumentService/App/Clients/PdfClient.cs
+++ DocumentService/App/Clients/PdfClient.cs
@@ -11,6 +11,7 @@ public class PdfClient
         var json = System.Text.Json.JsonSerializer.Serialize(documentText);
         var data = new StringContent(json, Encoding.UTF8, "application/json");
         
+        // TODO: Use DI for this instead to not create a new client per call...
         using var client = new HttpClient();
         var result = await client.PostAsync($"http://localhost:5056/Pdf?documentNumber={documentNumber}&customerNumber={customerNumber}", data);
         return result;
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # SEB_case
-Currently not 100% working due to unsupported packages for the PDF-converter.
+This microservice provides support for getting a document with an assigned DocumentID, Personal number and Text.
+
+Currently not 100% working on macOS due to unsupported packages in for the PDF-converter...
+
 To build and run use "docker compose up" for the docker-compose.yml in DocumentService/App folder.
+Use the GET endpoint in the DocumentService API to fetch your document.
